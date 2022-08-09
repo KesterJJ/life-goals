61491e4f9891de22643cfa9822486e895d1594d2
KesterJJ <kesterjjones@gmail.com>
2022-08-09 11:02:05 +0100

    PP-33 #done Mostly sorted HTML and CSS

---
+++ src/main/java/com/qa/lifegoals/exceptions/EndUserNotFoundException.java
@@ -0,0 +1,10 @@
+package com.qa.lifegoals.exceptions;
+
+import javax.swing.text.BadLocationException;
+
+public class EndUserNotFoundException extends BadLocationException {
+	public EndUserNotFoundException(String s, int offs) {
+		super(s, offs);
+		// TODO Auto-generated constructor stub
+	}
+}
